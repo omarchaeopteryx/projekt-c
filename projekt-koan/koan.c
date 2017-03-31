6c3c26b9e9ca1eb81fc9e3a79a0b3b7e2bc2d124
omarchaeopteryx <omaralimalik@gmail.com>
2017-03-30 17:21:32 -0700

    Update dictionary of phrases

--- projekt-koan/koan.c
+++ projekt-koan/koan.c
@@ -6,17 +6,17 @@ int main(void)
 {
    // Storing the dictionary of Zen koans.
    char a[6][100]; // <-- Note we must pre-set the size.
-   strcpy(a[0], "Odd and ever are on one die.");
+   strcpy(a[0], "Odd and even are on one die.");
    strcpy(a[1], "What is the sound of one hand clapping?");
    strcpy(a[2], "To study Buddhism is to study ourselves. To study ourselves is to forget ourselves.");
-   strcpy(a[3], "Koan four");
-   strcpy(a[4], "Koan five");
-   strcpy(a[5], "Koan six");
+   strcpy(a[3], "Of what use is it to discuss how grass and trees become enlightened?");
+   strcpy(a[4], "Tell me how many beans I hold in this hand!");
+   strcpy(a[5], "To a sincere student, every day is a fortunate day.");
 
    // Run the code.
   printf("You seek Zen!\n\n%s\n", a[0]);
   
-  // Terminate the program..
+  // Terminate the program.
   exit(0);
 }
 
